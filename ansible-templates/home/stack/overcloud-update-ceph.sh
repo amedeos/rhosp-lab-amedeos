d4567925bff4431e2350975f7eea9068840fce4b
Amedeo Salvati <amedeo@linux.com>
2020-10-05 01:08:39 +0200

    fix ceph node name in the update

--- ansible-templates/home/stack/overcloud-update-ceph.sh
+++ ansible-templates/home/stack/overcloud-update-ceph.sh
@@ -5,5 +5,5 @@ LOG_DIR=/home/stack/log-deploy
 mkdir -p ${LOG_DIR}
 
 source /home/stack/stackrc
-time openstack overcloud update run --nodes CephStorage | tee -a ${LOG_DIR}/overcloud-update-ceph-${LOG_DATE}.log
+time openstack overcloud update run --nodes Ceph | tee -a ${LOG_DIR}/overcloud-update-ceph-${LOG_DATE}.log
 
